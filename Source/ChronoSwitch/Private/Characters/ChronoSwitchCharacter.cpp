c10fb54fc0a02f0811977285affee2a0e3cb9a66
Nicolas <nicolas.mancone11@gmail.com>
2025-12-23 16:23:18 +0100

    C++ basic Character setup, BP classi core e basic Enhanced Input

--- Source/ChronoSwitch/Private/Characters/ChronoSwitchCharacter.cpp
+++ Source/ChronoSwitch/Private/Characters/ChronoSwitchCharacter.cpp
@@ -2,6 +2,11 @@
 
 
 #include "ChronoSwitch/Public/Characters/ChronoSwitchCharacter.h"
+#include "GameFramework/PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 
 
 // Sets default values
@@ -9,13 +14,39 @@ AChronoSwitchCharacter::AChronoSwitchCharacter()
 {
 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	
+	// Create and setup camera
+	FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCameraComponent->SetRelativeLocation(FVector(0.0f, 0.0f, 64.0f));
+	FirstPersonCameraComponent->bUsePawnControlRotation = true;
+	
+	// Create first person mesh
+	FirstPersonMeshComponent = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FirstPersonMesh"));
+	FirstPersonMeshComponent->SetupAttachment(FirstPersonCameraComponent);
+	FirstPersonMeshComponent->SetOnlyOwnerSee(true);
+	FirstPersonMeshComponent->bCastDynamicShadow = false;
+	FirstPersonMeshComponent->CastShadow = false;
+	
+	// Hide third-person mesh for owning player
+	GetMesh()->SetOwnerNoSee(true);
 }
 
 // Called when the game starts or when spawned
 void AChronoSwitchCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	if (const APlayerController* PC = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			if (DefaultMappingContext)
+			{
+				Subsystem->AddMappingContext(DefaultMappingContext, 0);
+			}
+		}
+	}
 }
 
 // Called every frame
@@ -28,5 +59,53 @@ void AChronoSwitchCharacter::Tick(float DeltaTime)
 void AChronoSwitchCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		if (MoveAction)
+		{
+			EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AChronoSwitchCharacter::Move);
+		}
+
+		if (LookAction)
+		{
+			EnhancedInput->BindAction(LookAction, ETriggerEvent::Triggered, this, &AChronoSwitchCharacter::Look);
+		}
+
+		if (JumpAction)
+		{
+			EnhancedInput->BindAction(JumpAction, ETriggerEvent::Started, this, &AChronoSwitchCharacter::JumpStart);
+			EnhancedInput->BindAction(JumpAction, ETriggerEvent::Completed, this, &AChronoSwitchCharacter::JumpStop);
+		}
+	}
 }
 
+void AChronoSwitchCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+	if (Controller)
+	{
+		AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+		AddMovementInput(GetActorRightVector(), MovementVector.X);
+	}
+}
+
+void AChronoSwitchCharacter::Look(const FInputActionValue& Value)
+{
+	FVector2D LookAxisValue = Value.Get<FVector2D>();
+	if (Controller)
+	{
+		AddControllerYawInput(LookAxisValue.X);
+		AddControllerPitchInput(LookAxisValue.Y);
+	}
+}
+
+void AChronoSwitchCharacter::JumpStart()
+{
+	Jump();
+}
+
+void AChronoSwitchCharacter::JumpStop()
+{
+	StopJumping();
+}
--- Source/ChronoSwitch/Public/Characters/ChronoSwitchCharacter.h
+++ Source/ChronoSwitch/Public/Characters/ChronoSwitchCharacter.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "ChronoSwitchCharacter.generated.h"
 
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+
 UCLASS()
 class CHRONOSWITCH_API AChronoSwitchCharacter : public ACharacter
 {
@@ -14,11 +18,48 @@ class CHRONOSWITCH_API AChronoSwitchCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AChronoSwitchCharacter();
+	
+	
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+	// Camera and mesh components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponent;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Mesh, meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* FirstPersonMeshComponent;
+	
+	// Input Mapping Context
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+	
+	// Input Action
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> MoveAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> LookAction;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Input)
+	TObjectPtr<UInputAction> JumpAction;
+	
+	// Input calback functions
+	UFUNCTION()
+	void Move(const FInputActionValue& Value);
+	
+	UFUNCTION()
+	void Look(const FInputActionValue& Value);
+	
+	UFUNCTION()
+	void JumpStart();
+	
+	UFUNCTION()
+	void JumpStop();
+	
+	
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
