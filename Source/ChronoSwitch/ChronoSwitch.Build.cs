0882e7fe51aa120970be8c27737b1c9e5f5825b5
seaglasses14 <luca.degennaro0302@gmail.com>
2026-02-24 17:25:36 +0100

    Changed Ini + Socket Plugin

--- Source/ChronoSwitch/ChronoSwitch.Build.cs
+++ Source/ChronoSwitch/ChronoSwitch.Build.cs
@@ -11,6 +11,8 @@ public class ChronoSwitch : ModuleRules
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		
+		DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
